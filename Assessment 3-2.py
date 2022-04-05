463a5027a8b67fb7b676344b148887a305682405
ashensamarakkodige <ashen.samarakkodig@live.vu.edu.au>
2022-04-05 11:11:54 +1000

    make separate array for only counteies names

--- Assessment 3-2.py
+++ Assessment 3-2.py
@@ -4,6 +4,8 @@ file =  open('./orders.csv', 'r')
 
 reader = csv.reader(file)
 
+countryArry = []
+
 for row in reader:
     if(row[8] != 'ship_country'):
-        print(row[8])
+        countryArry.append(row[8])

1fb2a2a4c65428570dc5d2ea55b24552f78fee47
ashensamarakkodige <ashen.samarakkodig@live.vu.edu.au>
2022-04-05 11:17:30 +1000

    finalize assessment 03 part 2

--- Assessment 3-2.py
+++ Assessment 3-2.py
@@ -10,4 +10,8 @@ for row in reader:
     if(row[8] != 'ship_country'):
         countryArry.append(row[8])
 
+# the most orders country
 print('The most orders: '+max(countryArry))
+
+# lowest orders country if need
+# print('The lowest orders: '+min(countryArry))
